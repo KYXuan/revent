7084cc03a46887756b497c3be9c4cb5025c1de58
Keyu Xuan <xuankeyu@gmail.com>
2019-03-15 17:47:23 -0400

    setup and initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        wdwqdwq
+      </div>
+    );
+  }
+}
+
+export default App;
